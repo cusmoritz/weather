669779a23112102491fb29c25275efbbd964832f
cusmoritz <cus.moritz@gmail.com>
2023-06-28 18:02:31 -0600

    feat: making calls client side

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { SearchBar } from "./components/SearchBar";
 import { useEffect } from "react";
-// import { locationCall } from './api'
+const cors = require('cors');
+console.log(module)
+// import { locationGuess } from './api'
 
 const handleLocationLoad = async () => {
     console.log('hello now.');
     // await locationCall();
 };
 
+const call = () => {fetch('https://www.googleapis.com/geolocation/v1/geolocate?key=', { 
+	method: "POST" ,
+	body: { 
+		name: "myDummyName" 
+	} 
+}) 
+.then(resp => resp.json()) 
+.then(resp => console.log('response, ', resp))
+.catch(err => console.log("An error occured :" + err));
+}
+call(cors);
 export const App = () => {
 
     useEffect(() => {
-        handleLocationLoad();
+        call();
     }, [])
 
     return (
--- src/api.js
+++ src/api.js
@@ -1,29 +1,31 @@
-// const express = require('express');
-// const cors = require('cors');
+console.log('api', module)
 
-// const server = express();
+const express = require('express');
+const cors = require('cors');
 
-// server.use(express.json());
+const server = express();
 
-// server.use(cors());
+server.use(express.json());
 
-// const googleApi = "secret";
+server.use(cors());
 
-// // start the server
-// server.listen(3000, () => {
-//     console.log('server listening')
+const googleApi = "secret";
 
-// });
+// start the server
+server.listen(3000, () => {
+    console.log('server listening')
 
-// const locationCall = async() => {
-//     try {
-//         const locationGuess = await fetch(googleApi);
-//         console.log('location', locationGuess)
-//     } catch (error) {
-//         console.log('there was a problem making a location call')
-//     }
-// }
+});
 
-// module.exports = {
-//     locationCall,
-// }
+const locationGuess = async() => {
+    try {
+        const guess = await fetch(googleApi);
+        console.log('location', guess)
+    } catch (error) {
+        console.log('there was a problem making a location call')
+    }
+};
+
+module.exports = {
+    locationGuess,
+}
