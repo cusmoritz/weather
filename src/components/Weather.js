11bfe74b4b353bd92a99351b69463c61ad2c234d
cusmoritz <cus.moritz@gmail.com>
2023-07-01 16:27:41 -0600

    feat: adding weather images folder, creating css templates for backgrounds

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
+import lightRain from '../images/light-rain.jpeg';
+import heavyRain from '../images/heavy_rain.jpeg'
 
 export const Weather = ({hourlyWeather, standardWeather}) => {
     console.log('standard: ', standardWeather)
+    console.log('hourly: ', hourlyWeather)
     return (
         <div className="weather-container">
             <span>Daily</span><span>Hourly</span>
             {standardWeather.map((segment) => {
                 return (
-                    <div className={`weather-segment ${segment.temperature}`} key={segment.endTime}>
+                    <div className={`weather-segment ${segment.temperature}`} key={segment.endTime} >
                         <h5>{segment.name}: {segment.temperature} degrees</h5>
                         <p>{segment.shortForecast}.</p>
                         <p>{segment.relativeHumidity.value} percent humidity.</p>
