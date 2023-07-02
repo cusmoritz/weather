25949a446b5ab43a55070f2bbec52793087dc11a
cusmoritz <cus.moritz@gmail.com>
2023-07-02 14:45:31 -0600

    feat: added weather photos, fixed background transparency overlay

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,27 @@
 import { useState, useEffect } from "react";
 import lightRain from '../images/light-rain.jpeg';
 import heavyRain from '../images/heavy_rain.jpeg'
+import sunny from '../images/sunny.jpeg'
+import chanceOfRain from '../images/chance_of_rain.jpeg'
+import partlyCloudy from '../images/partlyCloudy.jpeg'
+import thunderstorms from '../images/thunderstorms.jpeg'
+import mostlyClear from '../images/mostlyClear.jpeg'
+import mostlyCloudy from '../images/mostlyCloudy.jpeg'
 
 export const Weather = ({hourlyWeather, standardWeather}) => {
     console.log('standard: ', standardWeather)
     console.log('hourly: ', hourlyWeather)
+
+    const weatherImages = {
+        sunny,
+        lightRain,
+        heavyRain,
+        chanceOfRain,
+        partlyCloudy,
+        thunderstorms,
+        mostlyClear,
+
+    }
 
     const [weatherType, setWeatherType] = useState(true);
 
@@ -26,16 +43,44 @@ export const Weather = ({hourlyWeather, standardWeather}) => {
                     Hourly
                 </button>
             </div>
+            {/* "Chance Showers And Thunderstorms then Mostly Clear"
+            "Slight Chance Showers And Thunderstorms then Partly Cloudy"
+            "Partly Sunny then Chance Showers And Thunderstorms"
+            "Chance Showers And Thunderstorms"
+            "Slight Chance Rain Showers then Chance Showers And Thunderstorms"
+            "Chance Showers And Thunderstorms then Mostly Cloudy"
+            "Slight Chance Showers And Thunderstorms then Partly Cloudy"
+            "Partly Sunny then Chance Showers And Thunderstorms"
+            "Slight Chance Showers And Thunderstorms then Partly Cloudy" */}
+            
             {weatherType===true ? 
             standardWeather.map((segment) => {
+                if(segment.shortForecast.includes("Chance Showers And Thunderstorms")) {
+                    segment.icon=weatherImages.chanceOfRain
+                } else if (segment.shortForecast.includes("Sunny")) {
+                    segment.icon=weatherImages.sunny
+                } else if (segment.shortForecast.includes("Partly Sunny" || "Mostly Clear")) {
+                    segment.icon=weatherImages.partlyCloudy
+                } else if (segment.shortForecast.includes("Thunderstorms")) {
+                    segment.icon=weatherImages.thunderstorms
+                } else if (segment.shortForecast.includes("Chance Showers And Thunderstorms")) {
+                    segment.icon=weatherImages.mostlyCloudy
+                }
+
+                if (segment.probabilityOfPrecipitation.value == null){
+                    segment.probabilityOfPrecipitation.value = 0;
+                }
+                // `url(${segment.icon})`
                 return (
-                    <div className={`weather-segment ${segment.temperature}`} key={segment.endTime} >
+                    <div className={`weather-segment ${segment.temperature}`} key={segment.endTime} style={{
+                        backgroundImage: `url(${segment.icon})`
+                      }}>
                         {/* "https://api.weather.gov/icons/land/day/tsra_hi,30?size=medium"
                         "https://api.weather.gov/icons/land/day/few?size=medium"
                         "https://api.weather.gov/icons/land/day/bkn/tsra_sct,50?size=medium"
                         "https://api.weather.gov/icons/land/night/tsra_sct,50/tsra_sct,40?size=medium" */}
-                        <img className="weather-icon" src={segment.icon}/>
-                        <div className="centered">
+                        {/* <img className="weather-icon" src={segment.icon}/> */}
+                        <div className="weather-info"style={{backgroundImage: `linear-gradient(271deg, transparent, white)`}}>
                             <h5>{segment.name}: {segment.temperature} degrees</h5>
                             <p>{segment.shortForecast}.</p>
                             <p>{segment.relativeHumidity.value} percent humidity.</p>
@@ -52,7 +97,7 @@ export const Weather = ({hourlyWeather, standardWeather}) => {
                 return (
                     <div className={`weather-segment ${segment.temperature}`} key={segment.endTime} >
                         <img className="weather-icon" src={segment.icon}/>
-                        <h5>{segment.startTime}: {segment.temperature} degrees</h5>
+                        <h5>{segment.startTime.slice(11,16)} : {segment.temperature} degrees</h5>
                         <p>{segment.shortForecast}.</p>
                         <p>{segment.relativeHumidity.value} percent humidity.</p>
                         <p>{segment.probabilityOfPrecipitation.value} percent chance of precipitation.</p>
