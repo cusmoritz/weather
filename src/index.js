ce6c9e8679022f77f0554222a248172003d36dba
cusmoritz <cus.moritz@gmail.com>
2023-07-19 15:51:30 -0600

    chore: moved style to App.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import './App.css';
+// import './styles/app.css';
+
 
 const container = document.getElementById('app');
 
